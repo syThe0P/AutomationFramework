9a3be2816a2c7cbc805ebbe33c4333f308a70c42
Nitish Joon <nitish.joon@brevo.com>
2025-07-21 14:09:52 +0530

    renaming xml file

--- src/test/java/org/pom/tests/DropdownTest.java
+++ src/test/java/org/pom/tests/DropdownTest.java
@@ -1,10 +1,12 @@
 package org.pom.tests;
 
 import org.pom.base.BaseTest;
+import org.pom.listeners.ExtentReportListeners;
 import org.pom.pages.DropdownPage;
 import org.pom.utils.seleniumutils.WaitUtilities;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
+@Listeners(ExtentReportListeners.class)
 public class DropdownTest extends BaseTest{
 
     @Test(description = "verify dropdown functionality")
